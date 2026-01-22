c89c9299049de70c52c395614e09789bf298cd3b
Lee Bush <lee.burningbush@gmail.com>
2026-01-22 16:38:17 -0500

    Update Next Image config to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
